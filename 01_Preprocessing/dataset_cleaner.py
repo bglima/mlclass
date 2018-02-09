e868d70175c6defe94c448d1d83dead62d9db556
Nilson Sales <nilsonsaless@gmail.com>
2018-02-09 00:11:46 -0300

    Criando lista de colunas para ignorar na normalização

--- 01_Preprocessing/dataset_cleaner.py
+++ 01_Preprocessing/dataset_cleaner.py
@@ -22,10 +22,13 @@ for i in range(0, len(old_files) ):
     #df['DiabetesPedigreeFunction'] = (df['DiabetesPedigreeFunction']-df['DiabetesPedigreeFunction'].min())/(df['DiabetesPedigreeFunction'].max()-df['DiabetesPedigreeFunction'].min())
     #df['Insulin'] = (df['Insulin']-df['Insulin'].min())/(df['Insulin'].max()-df['Insulin'].min())
 
-    for j, key in enumerate(feature_cols):
-        if key != 'Pregnancies' and key != 'Age':
-            df[key] = (df[key] - df[key].min()) / (
-            df[key].max() - df[key].min())
+    to_ignore = ['Pregnancies', 'Age']
+    gen = (key for key in feature_cols if key not in to_ignore)
+
+    for key in gen:
+        print(key)
+        df[key] = (df[key] - df[key].min()) / (
+        df[key].max() - df[key].min())
 
 
 
