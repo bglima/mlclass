1e9c2aad393c201d03d37b80aab6c3c201714241
Nilson Sales <nilsonsaless@gmail.com>
2018-02-08 23:11:22 -0300

    Testando o Git no Pycharm e aditando o CSV de teste

--- 01_Preprocessing/dataset_cleaner.py
+++ 01_Preprocessing/dataset_cleaner.py
@@ -7,15 +7,16 @@ Created on Thu Feb  8 17:49:40 2018
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('old_diabetes_dataset.csv')
+#df = pd.read_csv('old_diabetes_dataset.csv')
+df = pd.read_csv('old_diabetes_app.csv')
 
 # carrega os nomes das colunas
 feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
                 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 
 # Normalizing csv
-df['DiabetesPedigreeFunction']=(df['DiabetesPedigreeFunction']-df['DiabetesPedigreeFunction'].min())/(df['DiabetesPedigreeFunction'].max()-df['DiabetesPedigreeFunction'].min())
-df['Insulin']=(df['Insulin']-df['Insulin'].min())/(df['Insulin'].max()-df['Insulin'].min())
+df['DiabetesPedigreeFunction'] = (df['DiabetesPedigreeFunction']-df['DiabetesPedigreeFunction'].min())/(df['DiabetesPedigreeFunction'].max()-df['DiabetesPedigreeFunction'].min())
+df['Insulin'] = (df['Insulin']-df['Insulin'].min())/(df['Insulin'].max()-df['Insulin'].min())
 
 # Medias
 average = {}
@@ -28,4 +29,5 @@ for key in feature_cols:
 
 
 # Saving CSV
-df.to_csv('diabetes_dataset.csv', index=False, header=True)
+#df.to_csv('diabetes_dataset.csv', index=False, header=True)
+df.to_csv('diabetes_app.csv', index=False, header=True)
