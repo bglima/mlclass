801dd513e185c8ab26daadace4bdf5d96429d293
Bruno Lima <bgcl@ic.ufal.br>
2018-03-08 17:16:35 -0300

    Fixing bugs

--- 02_Optimization/teste.py
+++ 02_Optimization/teste.py
@@ -24,78 +24,55 @@ def step(angles):
 def saveMaxAngle(max_angle, max_gain):
     dirname = os.path.dirname(os.path.abspath(__file__))
     filename = os.path.join(dirname, 'max.json')
+    curr_gain_dict = createDict(max_angle, max_gain)
 
+    try:
+        last_gain_dict = json.load(open(filename))
+        print('File found!')
+        if last_gain_dict.get('max_gain') < curr_gain_dict.get('max_gain'):
+            print('Saving new gain and angles')
+            json.dump(curr_gain_dict, open(filename, 'w'))
 
-    d = createDict(max_angle, max_gain)
-
-    if not (os.path.exists(filename)):
-        print('Diretório nao existente!')
-        json.dump(d, open(filename, 'w'))
-    else:
-        old_d = json.load(open(filename))
-        if old_d.get('max_gain') and (float(d.get('max_gain')) >= float(old_d.get('max_gain'))):
-            json.dump(d, open(filename, 'w'))
-            print('Máximo absoluto encontrado!')
-        else:
-            print('O tamanho máximo dessa rodada não é o máximo absoluto.\nMáximo absoluto: {}'.format(old_d.get('max_gain')))
+    except:
+        print('File not found! Creating new one.')
+        json.dump(curr_gain_dict, open(filename, 'w'))
 
 
 def createDict(max_angle, max_gain):
     return {'phi1':int(max_angle[0]),'theta1':int(max_angle[1]),'phi2':int(max_angle[2]),'theta2':int(max_angle[3]),'phi3':int(max_angle[4]),'theta3':int(max_angle[5]), 'max_gain':float(max_gain)}
 
-thresh_interval_array = [360, 180, 120, 80, 50, 45, 30, 20, 15, 8, 4, 2]
-angles = np.random.randint(thresh_interval_array[0], size=6)
-max_gain = 0
-prev_gain = 0
-new_angles = np.array([])
-max_angle = np.zeros((6,), dtype=np.int) #np.array([])
-curr_gain = step(angles)
-print('Acc for {} w/o thresh: {}'.format(angles, curr_gain))
-thresh = [0, 0, 0, 0, 0, 0]
+def main():
+    thresh_interval_array = [360, 180, 120, 80, 50, 45, 30, 20, 15, 8, 4, 2]
+    angles = np.random.randint(thresh_interval_array[0], size=6)
+    max_gain = 0
+    prev_gain = 0
+    new_angles = np.array([])
+    max_angle = np.zeros((6,), dtype=np.int)
+    curr_gain = step(angles)
+    print('Acc for {} w/o thresh: {}'.format(angles, curr_gain))
+    thresh = [0, 0, 0, 0, 0, 0]
 
-""" Things TO DO:
-    - Get best trial of N and show to user
-    - Define when to reduce thresh_interval (e.g. whenever precision goes further required value; do N trials)
-    - Find a way of not getting stuck into a local minimum
-    - Do it in parallel with K local minimun
-"""
+    for thresh_interval in thresh_interval_array:
+        for i in range(1500):
+            thresh = np.random.randint(thresh_interval, size=6) - int(thresh_interval / 2)
+            thresh = thresh.astype(int)
 
-# Max prec from [223 159 347  88 173 212] with 25.83223665952505
-# Max prec from [228 159 344  87 181 208] with 28.1164302338536
-# Max prec from [234 162 344  85 181 205] with 28.997204205603033
-# Max prec from [225, 167, 347, 79, 181, 203] with 29.48429448699448
-# Max prec from [229, 170, 347, 75, 180, 205] with 30.100344823569362
-# Max prec from [230, 170, 348, 73, 181, 205] with 30.24811445417071
-# Max prec from [230, 170, 348, 71, 180, 205] with 30.352217673641405
-# Max prec from [231, 171, 349, 71, 180, 205] with 30.43005389127427
-# Max prec from [231, 171, 349, 70, 180, 205] with 30.470835576196286
+            new_angles = angles + thresh
+            for i in range(6):
+                new_angles[i] = new_angles[i] % 360
 
-for thresh_interval in thresh_interval_array:
+            prev_gain = curr_gain
+            curr_gain = step(new_angles)
 
-    for i in range(1000):
-        # Try near combination of values if the precision is high
-        # Risk of falling into a local minimum
-        thresh = np.random.randint(thresh_interval, size=6) - int(thresh_interval / 2)
-        thresh = thresh.astype(int)
+            if curr_gain > max_gain:
+                max_gain = curr_gain
+                max_angle = new_angles
 
-        # Add thresh to previous angles
-        new_angles = angles + thresh
-        #print('New angles are: ', new_angles)
-        for i in range(6):
-            new_angles[i] = new_angles[i] % 360
+        print('\nMax prec from [{}, {}, {}, {}, {}, {}] with {}'.format(max_angle[0], max_angle[1], max_angle[2],max_angle[3],max_angle[4],max_angle[5], max_gain))
+        angles = max_angle
 
+    saveMaxAngle(max_angle, max_gain)
 
-        prev_gain = curr_gain
-        curr_gain = step(new_angles)
-
-        if curr_gain > max_gain:
-            max_gain = curr_gain
-            max_angle = new_angles
-
-        #print('Acc for {} is {}'.format(new_angles, curr_gain))
-        #print(new_angles)
-
-    print('\nMax prec from [{}, {}, {}, {}, {}, {}] with {}'.format(max_angle[0], max_angle[1], max_angle[2],max_angle[3],max_angle[4],max_angle[5], max_gain))
-    angles = max_angle
-
-saveMaxAngle(max_angle, max_gain)
+if __name__ == '__main__':
+    for i in range(5):
+        main()
