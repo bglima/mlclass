197f82f7c3d484d62c251e96fc6608166b650dfb
Nilson Sales <nilsonsaless@gmail.com>
2018-05-21 22:10:38 -0300

    Plotting success rate

--- 04_Reinforcement/markov.py
+++ 04_Reinforcement/markov.py
@@ -3,33 +3,45 @@ Created on 21 de mai de 2018
 
 Objetivo: Provar a solução do problema de Monty Hall utilizando aprendizagem por reforço
 
-@author: Nilson Sales
+@authors: Bruno Gabriel
+          João Paulo Clarindo
+          Nilson Sales
 '''
 
+
 import random as rand
+import matplotlib.pyplot as plt
+
+
+def plot(y):
+    plt.plot(y)
+    plt.ylabel('Success rate')
+    plt.xlabel('Iterations')
+    plt.show()
 
 
 def do_not_change(n_doors, trials):  # Caso o jogador não troque a porta
-    rating = 0
     success = 0
+    success_rates = []
 
     for i in range(1, trials):
         correct_door = rand.randint(1, n_doors)
         player_door = rand.randint(1, n_doors)
 
         if (correct_door == player_door):  # Caso o a porta do jogador seja a correta
-            rating += 2  # houve sucesso
             success += 1
-        else:
-            rating -= 1
 
-    print("Sem troca\nNúmero de tentativas:{}\nNota:{}\nPorcentagem:{}\n".format(trials, rating,
-                                                                                              success / trials))
+        success_rates.append(success/i)
+
+    print("Sem troca\nNúmero de tentativas:{}\nAcertos:{}\nPorcentagem:{}\n".format(trials, success,
+                                                                                       success / trials))
+    plot(success_rates)
 
 
 def do_change(n_doors, trials): # Caso o jogador troque a porta
     rating = 0
     success = 0
+    success_rates = []
 
     for i in range(1, trials):
 
@@ -52,17 +64,17 @@ def do_change(n_doors, trials): # Caso o jogador troque a porta
         player_door = keep_closed
 
         if (correct_door == player_door):  # Caso o a porta do jogador seja a correta
-            rating += 2  # houve sucesso
-            success += 1
-        else:
-            rating -= 1
+            success += 1  # houve sucesso
 
-    print("Com troca\nNúmero de tentativas:{}\nNota:{}\nPorcentagem:{}\n".format(trials, rating,
-                                                                                 success / trials))
+        success_rates.append(success/i)
+
+    print("Com troca\nNúmero de tentativas:{}\nAcertos:{}\nPorcentagem:{}\n".format(trials, success,
+                                                                                 success/trials))
+    plot(success_rates)
 
 
 n_doors = 3  # número de portas
-trials = 10000  # número de tentativas
+trials = 1000  # número de tentativas
 
 do_not_change(n_doors, trials)
 
