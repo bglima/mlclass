29e82eb5be73d0c13acd54a94572bee7330ca6ca
Nilson Sales <nilsonsaless@gmail.com>
2018-05-23 19:30:20 -0300

    Showing the best policy

--- 04_Reinforcement/montecarlo.py
+++ 04_Reinforcement/montecarlo.py
@@ -25,7 +25,7 @@ def init_doors(n_doors):
     return (correct_door, player_door)
 
 # Deixa apenas duas portas abertas: a inicial do jogador e mais uma. Uma delas deve conter o prêmio
-def close_doors(player_door, correct_door, n_doors):
+def open_doors(player_door, correct_door, n_doors):
         # Criando lista com as outras portas disponíveis
         other_doors = list(range(1, n_doors+1))
         # Abre outra(s) porta(s) e deixa uma fechada
@@ -67,25 +67,25 @@ def evaluate_policy_dt( policy, n_doors, trials ):
     success = 0         # Quantidade de acertos totais nos episódios
          
     # Repete para cada tentativa
-    for i in range (1, trials):                
+    for i in range(1, trials):                
         (correct_door, player_door) = init_doors( n_doors )
-        keep_closed = close_doors(player_door, correct_door, n_doors )
+        montys_door = open_doors(player_door, correct_door, n_doors)
         
         # Execute a ação definida na política
         if policy == 'ALWAYS_CHANGE':
-            (player_door, keep_closed) = change_doors(player_door, keep_closed, True)      
-        elif policy == 'CHANGE_EACH_2':
-            (player_door, keep_closed) = alternate_each_n(player_door, keep_closed, 2, i)
-        elif policy == 'CHANGE_EACH_5':
-            (player_door, keep_closed) = alternate_each_n(player_door, keep_closed, 5, i)    
-        elif policy == 'CHANGE_EACH_10':
-            (player_door, keep_closed) = alternate_each_n(player_door, keep_closed, 10, i)  
+            (player_door, montys_door) = change_doors(player_door, montys_door, True)
+        elif policy == 'CHANGE_EVERY_2':
+            (player_door, montys_door) = alternate_each_n(player_door, montys_door, 2, i)
+        elif policy == 'CHANGE_EVERY_5':
+            (player_door, montys_door) = alternate_each_n(player_door, montys_door, 5, i)
+        elif policy == 'CHANGE_EVERY_10':
+            (player_door, montys_door) = alternate_each_n(player_door, montys_door, 10, i)
         elif policy == 'NEVER_CHANGE':            
-            (player_door, keep_closed) = change_doors(player_door, keep_closed, False)
+            (player_door, montys_door) = change_doors(player_door, montys_door, False)
         elif policy == 'RANDOM_POLICY':
-            (player_door, keep_closed) = change_random(current_door, another_door)
+            (player_door, montys_door) = change_random(player_door, montys_door)
         else:
-            print('Policy not known... Please choose a valid one!\n')
+            print('Unknown policy... :( Please choose a valid one!\n')
             return -1
         
         # Retorno dado pelo ambiente
@@ -96,7 +96,7 @@ def evaluate_policy_dt( policy, n_doors, trials ):
             
         # Atualiza o valor da política
         success += reinforcement            
-        V_policy_last = success / i # Média dos valores
+        V_policy_last = success / i  # Média dos valores
         V_policy_list.append(V_policy_last)
 
     return (success, V_policy_list)
@@ -104,8 +104,9 @@ def evaluate_policy_dt( policy, n_doors, trials ):
 def main():
     n_doors = 3     # Número de portas
     trials = 10000  # Número de tentativas
+    highest_success = 0
 
-    policies = ['ALWAYS_CHANGE','CHANGE_EACH_2', 'CHANGE_EACH_5', 'CHANGE_EACH_10', 'NEVER_CHANGE', 'RANDOM_POLICY']
+    policies = ['ALWAYS_CHANGE', 'CHANGE_EVERY_2', 'CHANGE_EVERY_5', 'CHANGE_EVERY_10', 'NEVER_CHANGE', 'RANDOM_POLICY']
 
     fig = plt.figure(figsize=(8, 6))
     ax = plt.subplot(111)
@@ -114,11 +115,18 @@ def main():
         (success, V_list) = evaluate_policy_dt( policy, n_doors, trials )
         print("Política: ", policy, "\nNúmero de tentativas:{}\nAcertos:{}\nPorcentagem:{}\n".format(trials, success, V_list[-1]))
         plot(V_list, policy, fig)
+
+        if(success > highest_success):
+            highest_success = success
+            best_policy = policy
+
+    print("The best policy is: ", best_policy)
     
     ax.legend(loc='lower center')
     plt.show()
     fig.savefig('V_plot.png')
-    
+
+
 if __name__ == '__main__':
     main()
 
